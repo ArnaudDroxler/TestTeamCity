feec8415f333aaef05f003c43c85c4413ef51ce1
PoopHip <arnaud.droxler@gmail.com>
2024-05-05 23:57:16 +0200

    edit settings

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,12 @@
-import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2019_2.project
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.version
+
+
+version = "2019.2"
 
 project {
     buildType(ChainConfigA)
