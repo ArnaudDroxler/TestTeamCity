cf0ac9743728d85ddd010ea0cd4352c20470e871
PoopHip <arnaud.droxler@gmail.com>
2024-05-06 00:06:02 +0200

    edit settings

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -5,6 +5,8 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.project
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.version
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2
 
 version = "2019.2"
 
